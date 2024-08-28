146a2065362bd0cba3c0bd72b1f9cddbef931395
Sai Phani Kumar <saiphanikumarbalijepalli@gmail.com>
2024-08-28 18:46:58 +0530

    varailables

---
+++ 03-conversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=ramesh
+PERSON2=suresh
+
+echo "$PERSON1: hi $PERSON2, how are you ?"
+echo "$PERSON2: hi $PERSON1, im good how about you ?"
+echo "$PERSON1 : im doing good, what up"
+echo "$PERSON1 : fine"
