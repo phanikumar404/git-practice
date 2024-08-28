f6f46fdbabfc5e6650de9245ad95bbf794c5f95c
Sai Phani Kumar <saiphanikumarbalijepalli@gmail.com>
2024-08-28 18:53:56 +0530

    04 variables

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: hi $PERSON2, how are you ?"
+echo "$PERSON2: hi $PERSON1, im good how about you ?"
+echo "$PERSON1 : im doing good, what up"
+echo "$PERSON1 : fine"
