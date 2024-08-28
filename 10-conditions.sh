028f3b5a13f6d112ca452ad5043cb2701db10489
Sai Phani Kumar <saiphanikumarbalijepalli@gmail.com>
2024-08-28 19:44:39 +0530

    08 datatypes

---
+++ 10-conditions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ $NUMBER -gt 20 ]   #gt,lt,eq, -ne
+
+then
+
+    echo "given number : $NUMBER is greater than 20"
+
+ else
+
+     echo "given number : $NUMBER is lessthan 20"
+
+ fi
+ 
+        
